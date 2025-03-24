53810e2955ee864c5a98571ed1117925babca38a
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2025-03-24 11:09:47 -0300

    att

--- settings/urls.py
+++ settings/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('feedback/', include('feedbacks.urls')),
     path('servicos/', include('servicos.urls')),
 ]
