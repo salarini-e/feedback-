ce995b7513491ea56f83b6cfd5b045cfe222e93d
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2025-03-25 13:50:51 -0300

    att

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Feedback
 
 # Register your models here.
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('id', 'local', 'dt_inclusao', 'ambiente', 'tempo_espera', 'satisfacao', 'atendimento', 'incluir_contato')
-    list_filter = ('local', 'dt_inclusao', 'satisfacao', 'atendimento')
-    search_fields = ('local__nome', 'contato_nome', 'contato_telefoe')
-    ordering = ('-dt_inclusao',)
+# @admin.register(Feedback)
+# class FeedbackAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'local', 'dt_inclusao', 'ambiente', 'tempo_espera', 'satisfacao', 'atendimento', 'incluir_contato')
+#     list_filter = ('local', 'dt_inclusao', 'satisfacao', 'atendimento')
+#     search_fields = ('local__nome', 'contato_nome', 'contato_telefoe')
+#     ordering = ('-dt_inclusao',)
