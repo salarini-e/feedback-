67b1ef5caac67115c8a90d06f6a03209f05ca033
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2025-03-25 14:01:08 -0300

    att

--- servicos/views.py
+++ servicos/views.py
@@ -13,6 +13,9 @@ def list(request):
 
 @login_required
 def novo_atendimento(request):
+    if not request.user.is_superuser:
+        return redirect('servicos:index')
+    
     if request.method == 'POST':
         form = LocalDeAtendimentoForm(request.POST, request.FILES)  # Handle file uploads
         if form.is_valid():
@@ -24,6 +27,8 @@ def novo_atendimento(request):
 
 @login_required
 def editar_atendimento(request, hash):
+    if not request.user.is_superuser:
+        return redirect('servicos:index')    
     local = get_object_or_404(Local_de_atendimento, hash=hash)
     if request.method == 'POST':
         form = LocalDeAtendimentoForm(request.POST, request.FILES, instance=local)  # Handle file uploads
