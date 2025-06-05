e5e4343a68529eb3e6f7b0886a67ced3e468770a
gustavodias <gustavou.dias@gmail.com>
2025-06-05 11:53:48 -0300

    UPDATE: Adiciona nome e detalhamento ao botwhatsapp

--- botwhatsapp/models.py
+++ botwhatsapp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class AvaliacaoAtendimento(models.Model):
     data = models.DateTimeField(auto_now_add=True)
+    nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=15)
     avaliacao = models.IntegerField(choices=[(1, '1 - Satisfeito'), (2, '2 - Regular'), (3, '3 - Insatisfeito')])
-
+    detalhamento = models.TextField(blank=True, null=True)
     def __str__(self):
         return f"{self.telefone} - {self.avaliacao} - {self.data.strftime('%Y-%m-%d %H:%M:%S')}"
